952c46650323d72d65d5a1cafed7b1eead7fd5ae
Trifanenko Dmitrij <dimkas71g@github.com>
2016-02-25 09:15:50 +0200

    add some manipulation with files

--- dagger-demo/src/main/java/com/dimkas71/twitter/demo/Tweeter.java
+++
@@ -1,8 +0,0 @@
-package com.dimkas71.twitter.demo;
-
-public class Tweeter {
-    public void tweet(String message) {
-        TwitterApi api = new TwitterApi();
-        api.postTweet(message);
-    }
-}
